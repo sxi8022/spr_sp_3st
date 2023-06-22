81c5a04d64719da0d75b797d1094f150bfed77c2
sxi8022 <sxi8022@gmail.com>
2023-06-22 17:11:16 +0900

    0622 유효성체크_이메일

--- src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -1,13 +1,21 @@
 package com.sparta.springauth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
+    //@Email
+    @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-Za-z0-9\\-]+$")
+    @NotBlank
     private String email;
     private boolean admin = false;
     private String adminToken = "";
